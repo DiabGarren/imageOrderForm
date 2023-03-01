097a51050e5f4d3f9415e492ec9f32258c23ad45
DiabGarren <garrendiab@gmail.com>
2023-03-01 23:50:15 +0200

    Added old user

--- library/connections.php
+++ library/connections.php
@@ -8,8 +8,8 @@ function orderFormConnect()
 {
     $server = 'localhost:3308';
     $dbname = 'orderform';
-    $username = 'formClient';
-    $password = 'FJB9AQPZERIH.l0N';
+    $username = 'iClient';
+    $password = '@PPRyGKWqncEya2!';
     $dsn = "mysql:host=$server;dbname=$dbname";
     $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
